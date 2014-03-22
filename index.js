1b2ab3ffba39af51a5ecc88dcb9f276924315827
Václav Oborník <register.ufon@gmail.com>
2014-03-22 08:44:26 +0100

    repaired

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-module.exports = function(db,name,opts) {
+module.exports = function (db, name, opts) {
   var seq = {
-    db : db,
-    name : name,
-    opts : opts
+    db: db,
+    name: name,
+    opts: opts
   };
   var collection = db.collection(seq.opts && seq.opts.collname ? seq.opts.collname : 'counters');
-  seq.getNext = function(cb) {
-    function mycb(err,el) { if (err) {cb(err) } else {cb(null,el.sequence)} };
-    var collection = db.collection(seq.opts && seq.opts.collname ? seq.opts.collname : 'counters');
-    collection.findAndModify({
-      query: { _id: name },
-      update: { $inc: { sequence: 1 } },
-      new: true,
-      upsert : true
-    }, function(err,obj) {
-      if (err) {
-        cb(err)
-      }
-      else {
-        cb(null,obj.sequence)
-      } 
-    });
-  }
-  collection.insert({_id : name, sequence : 0 }, function(err){
-    if (err && (err.code >= 11000 || err.code <11005)) { 
+  seq.getNext = function (cb) {
+    collection.findAndModify(
+      { _id: name },
+      [['_id', 'asc']],
+      { $inc: { sequence: 1 } },
+      { new: true, upsert: true },
+      function (err, obj) {
+        if (err) {
+          cb(err)
+        } else {
+          cb(null, obj.sequence)
+        }
+      });
+  };
+  collection.insert({_id: name, sequence: 0 }, function (err) {
+    if (err && (err.code >= 11000 && err.code <= 11001)) {
       // this should be ok according to http://docs.mongodb.org/manual/tutorial/create-an-auto-incrementing-field/
     }
-    else if (err){
+    else if (err) {
       throw err;
     }
   });
